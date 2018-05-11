c8c189f02ac069b09c4834b8eed5643bbf0129e1
oliag <oliag@microsoft.com>
2018-05-11 16:22:07 -0700

    Added column attributes to make it work with the latest nuget version

--- GitHubLabeler/GitHubLabeler/CoreIssue.cs
+++ GitHubLabeler/GitHubLabeler/CoreIssue.cs
@@ -4,11 +4,16 @@ namespace GitHubLabeler
 {
     class CoreFxIssue
     {
+        [Column(ordinal: "0")]
         public string ID;
+        [Column(ordinal: "1")]
         public string Area; // This is an issue label, for example "area-System.Threading"
+        [Column(ordinal: "2")]
         public string Title;
+        [Column(ordinal: "3")]
         public string Description;
 
+        [Column(ordinal: "4")]
         [ColumnName("Label")]
         public string Label;
     }
